1b6af76e4261608d8b3b3cbeed3b4c5aeb3d62fa
ycham12 <ycham1212@gmail.com>
2025-02-01 02:30:20 +0530

    added a sum function

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,8 @@
-print("adding all the data processing code related to fraud detection")
+print("adding all the data processing code related to fraud detection") 
+
+a = 5
+b = 7
+def sum(a,b):
+    c = a + b
+    return a
+print ("added a new function sum which is not correct")
